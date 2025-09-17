53552f2319a0a52cfd61e2ad4dfccf57ef67cc06
Mrinal Singh/CONTROLLER/ENGG/PUNE <msingh27@unominda.com>
2025-09-17 17:07:45 +0530

    sorting folder added

---
+++ src/sorting/Basic.java
@@ -0,0 +1,14 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class Basic {
+    public static void main(String[] args) {
+        int arr[] = {5,7,3,4,9,0};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+        char c[]= {'A','e','a','t','Y'};
+        Arrays.sort(c);
+        System.out.println(Arrays.toString(c));
+    }
+}
---
+++ src/sorting/Point.java
@@ -0,0 +1,25 @@
+package sorting;
+
+import java.util.Arrays;
+
+class Point implements Comparable<Point>{
+
+    int x ; int y;
+    Point(int x ,int y){
+        this.x = x ;
+        this.y = y;
+    }
+    @Override
+    public int compareTo(Point p) {
+        return this.x - p.x;
+    }
+}
+class Test{
+    public static void main(String[] args) {
+        Point arr[] = {new Point(10,20), new Point(3,12), new Point(5,7)};
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i].x +" "+arr[i].y);
+        }
+    }
+}
